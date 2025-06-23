9dc4cda02e94efde6b0d4b105dedef6c8f831a89
kokiSAT0 <kokisato0123@gmail.com>
2025-06-23 13:12:25 +0900

    feat: 枠線をグラデーション化

--- src/game/utils.ts
+++ src/game/utils.ts
@@ -53,7 +53,7 @@ export function applyDistanceFeedback(
     maxDist = Math.hypot(goal.x, goal.y),
     // ゴールから遠いとき 120ms, 近いとき 20ms 振動させます
     vibrateRange = [120, 20],
-    borderRange = [2, 20],
+    borderRange = [2, 40],
     showRange = [200, 1000],
   } = opts;
 
@@ -89,7 +89,7 @@ export function applyBumpFeedback(
 ) {
   const {
     maxDist = Math.hypot(goal.x, goal.y),
-    borderRange = [2, 20],
+    borderRange = [2, 40],
     showRange = [200, 1000],
   } = opts;
 
