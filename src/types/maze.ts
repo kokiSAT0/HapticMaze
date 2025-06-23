020c4fff5ce3c7705fc505ba84a4d5d4ac0efc34
kokiSAT0 <kokisato0123@gmail.com>
2025-06-23 09:03:17 +0900

    feat: add MiniMap component

---
+++ src/types/maze.ts
@@ -0,0 +1,13 @@
+export interface MazeData {
+  id: string;
+  size: 10;
+  start: [number, number];
+  goal: [number, number];
+  v_walls: [number, number][];
+  h_walls: [number, number][];
+}
+
+export interface Vec2 {
+  x: number;
+  y: number;
+}
