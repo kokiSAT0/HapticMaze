83f2afb72db8caaa0f0cf267acd78ff6b9ac353c
kokiSAT0 <kokisato0123@gmail.com>
2025-07-11 11:56:17 +0900

    feat: level1 enemy counts

--- src/game/__tests__/level1EnemyCounts.test.ts
+++ src/game/__tests__/level1EnemyCounts.test.ts
@@ -8,19 +8,19 @@ describe('level1EnemyCounts', () => {
     expect(level1EnemyCounts(1)).toEqual({ random: 1, slow: 0, sight: 0, fast: 0 });
   });
 
-  test('ステージ3ではランダム敵1体', () => {
-    expect(level1EnemyCounts(3)).toEqual({ random: 1, slow: 0, sight: 0, fast: 0 });
+  test('ステージ3では鈍足視認1体', () => {
+    expect(level1EnemyCounts(3)).toEqual({ random: 0, slow: 1, sight: 0, fast: 0 });
   });
 
   test('ステージ32ではランダム敵2体', () => {
     expect(level1EnemyCounts(32)).toEqual({ random: 2, slow: 0, sight: 0, fast: 0 });
   });
 
-  test('ステージ63では鈍足視認1体とランダム1体', () => {
-    expect(level1EnemyCounts(63)).toEqual({ random: 1, slow: 1, sight: 0, fast: 0 });
+  test('ステージ63では鈍足視認2体とランダム1体', () => {
+    expect(level1EnemyCounts(63)).toEqual({ random: 1, slow: 2, sight: 0, fast: 0 });
   });
 
-  test('ステージ100でも鈍足視認2体とランダム1体', () => {
-    expect(level1EnemyCounts(100)).toEqual({ random: 1, slow: 2, sight: 0, fast: 0 });
+  test('ステージ100では等速視認2体と鈍足視認2体', () => {
+    expect(level1EnemyCounts(100)).toEqual({ random: 1, slow: 2, sight: 2, fast: 0 });
   });
 });
